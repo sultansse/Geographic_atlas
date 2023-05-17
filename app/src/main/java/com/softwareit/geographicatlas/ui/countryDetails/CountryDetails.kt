9d3c4c5347b7c5ed1942a06d12212b31dd3a0a7e
Sultan <ss.softwareit@gmail.com>
2023-05-17 12:43:48 +0600

    FEA: geolocation clickable, opens google map

--- app/src/main/java/com/softwareit/geographicatlas/ui/countryDetails/CountryDetails.kt
+++ app/src/main/java/com/softwareit/geographicatlas/ui/countryDetails/CountryDetails.kt
@@ -1,11 +1,14 @@
 package com.softwareit.geographicatlas.ui.countryDetails
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -40,6 +43,7 @@ class CountryDetails : Fragment() {
         val args: CountryDetailsArgs by navArgs()
         val countryCode = args.countryCode
         val countryName = args.countryName
+        val mapLink = "https://goo.gl/maps/4F4PpDhGJgVvLby57"
 
         (requireActivity() as AppCompatActivity).supportActionBar?.title = countryName
 
@@ -49,16 +53,27 @@ class CountryDetails : Fragment() {
             binding.apply {
                 val country = it[0]
                 loadImgUrl(flagIv, country.flags.png)
-                capitalNameTv.text = getColoredText("&#8226; Capital: \n${country.capital?.get(0) ?: "No capital"}")
-                capitalCoordinatesTv.text = getColoredText("&#8226; Capital Coordinates: \n${country.capitalInfo.latlng}")
-                populationTv.text = getColoredText("&#8226; Population: \n${country.population}")
-                areaTv.text = getColoredText("&#8226; Area: \n${country.area} km²")
+                capitalNameTv.text =
+                    getColoredText("&#8226; Capital: ${country.capital?.get(0) ?: "No capital"}")
+                capitalCoordinatesTv.text =
+                    getColoredText("&#8226; Capital Coordinates: ${country.capitalInfo.latlng}")
+                populationTv.text = getColoredText("&#8226; Population: ${country.population}")
+                areaTv.text = getColoredText("&#8226; Area: ${country.area} km²")
                 val currencyString =
                     country.currencies?.entries?.joinToString("\n") { "${it.value.name} (${it.value.symbol}) (${it.key})" }
-                currenciesTv.text = getColoredText("&#8226; Currencies: \n$currencyString")
-                regionTv.text = getColoredText("&#8226; Region: \n${country.subregion ?: "No region"}")
-                timezonesTv.text = getColoredText("&#8226; Timezones: \n${country.timezones.joinToString("\n")}")
+                currenciesTv.text = getColoredText("&#8226; Currencies: $currencyString")
+                regionTv.text =
+                    getColoredText("&#8226; Region: ${country.subregion ?: "No region"}")
+                timezonesTv.text =
+                    getColoredText("&#8226; Timezones: ${country.timezones.joinToString("\n")}")
             }
+        }
+
+        binding.capitalCoordinatesTv.setOnClickListener {
+            Toast.makeText(requireContext(), "Opening Google Maps", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(mapLink))
+            intent.setPackage("com.google.android.apps.maps") // Specify the package name for Google Maps
+            startActivity(intent)
         }
 
     }
